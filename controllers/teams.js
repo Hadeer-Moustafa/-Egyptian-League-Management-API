1ea1c2a069ea604ef31af87f9ea51e81cc74d739
Hadeer Mostafa <Hadeer.Mostafa00@outlook.com>
2025-08-26 07:32:41 +0300

    updata teams controllers with asyncWrapper

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,81 +1,56 @@
 const teams = require("../models/teams&matchs").Team;
 const { SUCCESS, FAIL, ERROR } = require("../utilities/httpstatus");
+const asyncWrapper = require("../middleware/asyncWrapper");
 
-const getAllTeams = (req, res) => {
+const getAllTeams = asyncWrapper(async (req, res) => {
   let quary = req.query;
   const limit = quary.limit || 6;
   const page = quary.page || 1;
   const skip = (page - 1) * limit;
-  teams
-    .find({}, { __v: false })
-    .limit(limit)
-    .skip(skip)
-    .then((data) => {
-      res.json({ status: SUCCESS, data: { teams: data } });
-    });
-};
+  const data = await teams.find({}, { __v: false }).limit(limit).skip(skip);
 
-const getTeamById = (req, res) => {
-  teams
-    .findById(req.params.teamId, { __v: false })
-    .then((data) => {
-      if (data) {
-        res.json({ status: SUCCESS, data: { team: data } });
-      } else {
-        res
-          .status(404)
-          .json({ status: FAIL, data: { team: "team not found" } });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ status: ERROR, message: err.message });
-    });
-};
+  res.json({ status: SUCCESS, data: { teams: data } });
+});
 
-const addTeam = async (req, res) => {
-  try {
-    const newTeam = new teams(req.body);
+const getTeamById = asyncWrapper(async (req, res) => {
+  const data = await teams.findById(req.params.teamId, { __v: false });
 
-    await newTeam.save();
-    res.status(201).json({ status: SUCCESS, data: { team: newTeam } });
-  } catch (err) {
-    res.status(400).json({ status: ERROR, message: err.message });
+  if (data) {
+    res.json({ status: SUCCESS, data: { team: data } });
+  } else {
+    res.status(404).json({ status: FAIL, data: { team: "team not found" } });
   }
-};
+});
 
-const updateTeam = (req, res) => {
-  teams
-    .updateOne({ _id: req.params.teamId }, { $set: req.body })
-    .then((data) => {
-      if (data.matchedCount !== 0) {
-        res.json({ status: SUCCESS, data: data });
-      } else {
-        res
-          .status(404)
-          .json({ status: FAIL, data: { team: "team not found" } });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ status: ERROR, message: err.message });
-    });
-};
+const addTeam = asyncWrapper(async (req, res) => {
+  const newTeam = new teams(req.body);
 
-const deleteTeam = (req, res) => {
-  teams
-    .deleteOne({ _id: req.params.teamId })
-    .then((data) => {
-      if (data.deletedCount !== 0) {
-        res.json({ status: SUCCESS, data: null });
-      } else {
-        res
-          .status(404)
-          .json({ status: FAIL, data: { team: "team not found" } });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ status: ERROR, message: err.message });
-    });
-};
+  await newTeam.save();
+  res.status(201).json({ status: SUCCESS, data: { team: newTeam } });
+});
+
+const updateTeam = asyncWrapper(async (req, res) => {
+  const data = await teams.updateOne(
+    { _id: req.params.teamId },
+    { $set: req.body }
+  );
+
+  if (data.matchedCount !== 0) {
+    res.json({ status: SUCCESS, data: data });
+  } else {
+    res.status(404).json({ status: FAIL, data: { team: "team not found" } });
+  }
+});
+
+const deleteTeam = asyncWrapper(async (req, res) => {
+  const data = await teams.deleteOne({ _id: req.params.teamId });
+
+  if (data.deletedCount !== 0) {
+    res.json({ status: SUCCESS, data: null });
+  } else {
+    res.status(404).json({ status: FAIL, data: { team: "team not found" } });
+  }
+});
 
 module.exports = {
   getAllTeams,
